3180ff411996f67a899db69518b1c68f0b907b03
Philip Mark Deazeta <pmdeazeta@facebook.com>
2011-05-25 16:22:34 +0800

    added Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110525061500) do
+ActiveRecord::Schema.define(:version => 20110525065119) do
 
   create_table "comments", :force => true do |t|
     t.integer  "story_id"
@@ -24,14 +24,6 @@ ActiveRecord::Schema.define(:version => 20110525061500) do
     t.integer  "user_id"
     t.integer  "project_id"
     t.string   "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "member_lists", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "project_id"
-    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -95,11 +87,23 @@ ActiveRecord::Schema.define(:version => 20110525061500) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.string   "role"
     t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
