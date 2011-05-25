6b95092d0de92404c26fa124199683a4a2186d68
normancapule <jndc6@yahoo.com>
2011-05-25 11:58:35 +0800

    Rails Initial Generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECCFinalProject::Application.config.secret_token = 'c9b034ad5c60a0f2c64b891a177fcb5356dd64f28008446b8fa76deecef894e70addda8d8769abc25e5c83166b2d448e0ee518196082506362dacb44605b9c32'
