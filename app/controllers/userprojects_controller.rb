581545d6d770283071329a690336e81907f3e744
Philip Mark Deazeta <pmdeazeta@facebook.com>
2011-05-26 15:27:47 +0800

    Add members

--- app/controllers/userprojects_controller.rb
+++ app/controllers/userprojects_controller.rb
@@ -1,41 +1,59 @@
 class UserprojectsController < ApplicationController
   def new
+    @project = Project.find(params[:project_id])
     @userproject = Userproject.new
+    @user = User.all
   end
 
   def create
-    @userproject = Userproject.new(params[:userproject])
+    
+    @project = Project.find(params[:project_id])
+    
+    params[:user_ids].each do |user_id|
+      @userproject = Userproject.new(:user_id => user_id, :project_id=> params[:project_id], :role=> params[:userproject][:role] )
+      @userproject.save
+    end
+    
+    
+    
     if @userproject.save
-      redirect_to @userproject, :notice => "Successfully created userproject."
+      redirect_to project_userprojects_path(@project), :notice => "Successfully added member."
     else
       render :action => 'new'
     end
   end
 
   def index
-    @userprojects = Userproject.all
+    @project = Project.find(params[:project_id])
+    @userprojects = @project.userprojects
+    @user = User.all
   end
 
   def show
+    @project = Project.find(params[:project_id])
     @userproject = Userproject.find(params[:id])
   end
 
   def destroy
-    @userproject = Userproject.find(params[:id])
+    @project = Project.find(params[:project_id])
+    @userproject = @project.userprojects.find params[:id]
     @userproject.destroy
-    redirect_to userprojects_url, :notice => "Successfully destroyed userproject."
+    redirect_to project_userprojects_path(@project), :notice => "Successfully destroyed member."
   end
-
+  
   def update
-    @userproject = Userproject.find(params[:id])
-    if @userproject.update_attributes(params[:userproject])
-      redirect_to @userproject, :notice  => "Successfully updated userproject."
+    @project = Project.find(params[:project_id])
+    @userproject = @project.userprojects.find params[:id]
+    if @userproject.update_attributes(params[:release])
+      redirect_to project_userproject_path(@project, @userproject), :notice  => "Successfully updated member list."
     else
       render :action => 'edit'
     end
   end
 
   def edit
+    @project = Project.find(params[:project_id])
     @userproject = Userproject.find(params[:id])
+    @user = User.all
   end
 end
