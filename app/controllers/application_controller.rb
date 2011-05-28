6fe83b3b0c3c52546e94135993426d2caa6ece26
normz <jndc6@yahoo.com>
2011-05-28 22:27:41 +0800

    Small fix to logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def log_action(project, user, content)
-    log = Log.create(:user_id => user.id, :project_id => project.id, :content=>content)
-    log.user = user
-    log.project = project
-    log.content = content
-    log.save
+    Log.create(:user_id => user.id, :project_id => project.id, :content=>content)
   end
 end
