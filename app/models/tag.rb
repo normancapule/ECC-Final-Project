0ba7c3aae24170b764c1a283dd726964b7cab685
normz <jndc6@yahoo.com>
2011-05-28 16:05:05 +0800

    Deletions

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   belongs_to :project
   belongs_to :story
-  validates_uniqueness_of :content, :scope => :project_id
+  validates_uniqueness_of :content
   validates_length_of :content, :maximum => 60
 end
