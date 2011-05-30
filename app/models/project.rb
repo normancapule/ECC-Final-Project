d6481b7075316cb0d62f5dc8111ff062821848d0
Philip Mark Deazeta <pmdeazeta@facebook.com>
2011-05-30 10:34:11 +0800

    Modified models and adding members

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,10 @@
 class Project < ActiveRecord::Base
-  has_many :userprojects
+  has_many :userprojects, :dependent => :destroy
   has_many :users, :through => :userprojects
-  has_many :releases
-  has_many :stories
-  has_many :tags
-  has_many :logs
+  has_many :releases, :dependent => :destroy
+  has_many :stories, :dependent => :destroy
+  has_many :tags, :dependent => :destroy
+  has_many :logs, :dependent => :destroy
   validates_presence_of :project_name
   validates_uniqueness_of :project_name
 end
--- app/models/release.rb
+++ app/models/release.rb
@@ -1,6 +1,6 @@
 class Release < ActiveRecord::Base
   belongs_to :project
-  has_many :stories
+  has_many :stories, :dependent => :destroy
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :project_id
 
